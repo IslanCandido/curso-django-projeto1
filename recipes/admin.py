0d29d4562e1864cc34b31a36db968ac554a9a59a
Islan Candido <islancandido03@gmail.com>
2022-04-03 11:52:47 -0300

    versão atualizada com ORM e Models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from recipes.models import Category, Recipe
+
+
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class RecipiAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Recipe, RecipiAdmin)
